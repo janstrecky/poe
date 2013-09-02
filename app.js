837ffcd06b1855f5e4744456dcad3fe742e53919
Morten Lindhardt <r3fuze@gmail.com>
2013-09-02 14:46:06 -0400

    test

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.configure(function() {
     app.set("views", __dirname + "/views");
     app.set("view engine", "jade");
 
-    app.set("view options", { layout: false });
+    // app.set("view options", { layout: false }); REMOVE
     // app.use(express.logger("dev")); // comment if too much spam
 
     app.use(express.bodyParser());
