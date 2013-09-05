39887f462be9e9acd47b8f2be5f30aa26aa45cb5
Morten Lindhardt <r3fuze@gmail.com>
2013-09-05 19:10:59 -0400

    Fixed mixpanel script so it passes lint

--- public/js/main.js
+++ public/js/main.js
@@ -3,17 +3,68 @@
 
 /* Initalize MixPanel
  * ====================== */
-(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===e.location.protocol?"https:":"http:")+'//cdn.mxpnl.com/libs/mixpanel-2.2.min.js';f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");2==a.length&&(b=b[a[0]],h=a[1]);b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)))}}var c=b;"undefined"!==
-typeof d?c=b[d]=[]:d="mixpanel";c.people=c.people||[];c.toString=function(b){var a="mixpanel";"mixpanel"!==d&&(a+="."+d);b||(a+=" (stub)");return a};c.people.toString=function(){return c.toString(1)+".people (stub)"};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++)f(c,i[g]);
-b._i.push([a,e,d])};b.__SV=1.2}})(document,window.mixpanel||[]);
+/*(function (e, b) {
+    if (!b.__SV) {
+        var a, f, i, g;
+        window.mixpanel = b;
+        a = e.createElement("script");
+        a.type = "text/javascript";
+        a.async = !0;
+        a.src = ("https:" === e.location.protocol ? "https:" : "http:") + "//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";
+        f = e.getElementsByTagName("script")[0];
+        f.parentNode.insertBefore(a, f);
+        b._i = [];
+        b.init = function (a, e, d) {
+            function f(b, h) {
+                var a = h.split(".");
+                if (a.length === 2) {
+                    b = b[a[0]];
+                    h = a[1];
+                }
+                // 2 == a.length && (b = b[a[0]], h = a[1]);
+                b[h] = function () {
+                    b.push([h].concat(Array.prototype.slice.call(arguments, 0)));
+                };
+            }
+            var c = b;
+            if (typeof d !== "undefined") {
+                c = b[d] = [];
+            } else {
+                d = "mixpanel";
+            }
+            //"undefined" !== typeof d ? c = b[d] = [] : d = "mixpanel";
+
+            c.people = c.people || [];
+            c.toString = function (b) {
+                var a = "mixpanel";
+                if (d !== "mixpanel")  {
+                    a += "." + d;
+                }
+                //"mixpanel" !== d && (a += "." + d);
+                if (!b) a += " (stub)";
+                // b || (a += " (stub)");
+                return a;
+            };
+            c.people.toString = function () {
+                return c.toString(1) + ".people (stub)";
+            };
+            i = "disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");
+            for (g = 0; g < i.length; g++) f(c, i[g]);
+            b._i.push([a, e, d]);
+        };
+        b.__SV = 1.2;
+    }
+})(document, window.mixpanel || []);*/
+
+(function(e,b){if(!b.__SV){var a,f,i,g;window.mixpanel=b;a=e.createElement("script");a.type="text/javascript";a.async=!0;a.src=("https:"===e.location.protocol?"https:":"http:")+"//cdn.mxpnl.com/libs/mixpanel-2.2.min.js";f=e.getElementsByTagName("script")[0];f.parentNode.insertBefore(a,f);b._i=[];b.init=function(a,e,d){function f(b,h){var a=h.split(".");if(a.length===2){b=b[a[0]];h=a[1];}b[h]=function(){b.push([h].concat(Array.prototype.slice.call(arguments,0)));};}var c=b;if(typeof d!=="undefined"){c=b[d]=[];}else{d="mixpanel";}c.people=c.people||[];c.toString=function(b){var a="mixpanel";if(d!=="mixpanel"){a+="."+d;}if(!b){a+=" (stub)";}return a;};c.people.toString=function(){return c.toString(1)+".people (stub)";};i="disable track track_pageview track_links track_forms register register_once alias unregister identify name_tag set_config people.set people.set_once people.increment people.append people.track_charge people.clear_charges people.delete_user".split(" ");for(g=0;g<i.length;g++){f(c,i[g]);}b._i.push([a,e,d]);};b.__SV=1.2;}})(document,window.mixpanel||[]);
 
 mixpanel.init("7f95e6ec220f636337ddcc78ec3b75f0");
 
 var favOptions = {
-    fillColor: '#000000',
-    fontFamily: 'Lato',
-    fontColor: '#FFFFFF',
-    text: 'PoE'
+    fillColor: "#000000",
+    fontFamily: "Lato",
+    fontColor: "#FFFFFF",
+    text: "PoE"
 };
 
 var favicon = new FontFavi(favOptions);
